878691c2312f53dae250daef3ebf1de384279ec8
Felipe Marques <flipmorsch@gmail.com>
2023-11-06 23:32:14 -0300

    add customer and wine entities

---
+++ src/domain/entities/customer.ts
@@ -0,0 +1,7 @@
+export type Customer = {
+  nome: string
+  cpf: string
+  telefone: string
+  tipo_vinho_preferido: string
+  quantidade_comprada: number
+}
---
+++ src/domain/entities/wine.ts
@@ -0,0 +1,6 @@
+export type Wine = {
+  tipo_vinho: string
+  preco: number
+  safra: string
+  ano_compra: number
+}
