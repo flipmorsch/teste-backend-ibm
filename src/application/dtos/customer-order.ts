91e2295dec62f37a1a7fcea46b415ccbaafc158f
Felipe Marques <flipmorsch@gmail.com>
2023-11-07 11:13:51 -0300

    add customer order DTO

---
+++ src/application/dtos/customer-order.ts
@@ -0,0 +1,11 @@
+export type CustomerOrder = {
+  nome: string
+  cpf: string
+  telefone: string
+  tipo_vinho: string
+  preco: number
+  quantidade_comprada: number
+  safra: string
+  ano_compra: number
+  total: number
+}
