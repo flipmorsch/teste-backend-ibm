068500dd6ff525f8239864b7a1636a4ed3164263
Felipe Marques <flipmorsch@gmail.com>
2023-11-07 00:00:13 -0300

    add customer service

---
+++ src/application/services/customer.service.ts
@@ -0,0 +1,13 @@
+import {config} from '../../config'
+import {Customer} from '../../domain/entities/customer'
+import {Http} from '../../infra/http'
+
+export class CustomerService {
+  private readonly customer_path = config.customer_path
+  constructor(private readonly http: Http) {}
+
+  async getCustomers() {
+    const customers = await this.http.get<Customer[]>(this.customer_path)
+    return customers
+  }
+}
