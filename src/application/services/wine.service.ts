5ef3f087bb06646f1dd3d134e71875abd513b52b
Felipe Marques <flipmorsch@gmail.com>
2023-11-07 00:00:23 -0300

    add wine service

---
+++ src/application/services/wine.service.ts
@@ -0,0 +1,13 @@
+import { config } from "../../config"
+import { Wine } from "../../domain/entities/wine"
+import { Http } from "../../infra/http"
+
+export class WineService {
+  private readonly wine_path = config.wine_path
+  constructor(private readonly http: Http) {}
+
+  async getWines() {
+    const wines = await this.http.get<Wine[]>(this.wine_path)
+    return wines
+  }
+}
