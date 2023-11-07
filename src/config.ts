0bae3b4134625a1afc809e4c5c0f67c6cb205a89
Felipe Marques <flipmorsch@gmail.com>
2023-11-06 23:59:10 -0300

    add a config file with environment variables

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const config = {
+  base_url: 'https://run.mocky.io',
+  customer_path: '/v3/07acae80-d898-4136-a803-4adc1a3131eb',
+  wine_path: '/v3/fb94d1e0-f157-4133-94d7-37de04d5a8e6',
+}
