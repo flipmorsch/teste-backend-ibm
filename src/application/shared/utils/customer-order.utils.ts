ec2899962efc0eeab823bab13944956de82fd684
Felipe Marques <flipmorsch@gmail.com>
2023-11-07 22:52:16 -0300

    change customer order utils due to mock change

--- src/application/shared/utils/customer-order.utils.ts
+++ src/application/shared/utils/customer-order.utils.ts
@@ -1,30 +1,30 @@
-import { Customer } from "../../../domain/entities/customer"
-import { Wine } from "../../../domain/entities/wine"
-import { CustomerOrder } from "../../dtos/customer-order"
+import {Customer} from '../../../domain/entities/customer'
+import {Wine} from '../../../domain/entities/wine'
+import {CustomerOrder} from '../../dtos/customer-order'
 
 export class CustomerOrderUtils {
-  static associateWinesToCustomers(
-    customers: Customer[],
-    wines: Wine[]
-  ): CustomerOrder[] {
+  static mapOrders(customers: Customer[], wines: Wine[]): CustomerOrder[] {
     const customerOrders: CustomerOrder[] = []
 
     customers.forEach(customer => {
-      const customerWine = wines.filter(
-        wine => wine.tipo_vinho === customer.tipo_vinho_preferido
-      )
-      customerWine.forEach(wine => {
-        customerOrders.push({
-          nome: customer.nome,
-          cpf: customer.cpf,
-          telefone: customer.telefone,
-          tipo_vinho: customer.tipo_vinho_preferido,
-          preco: wine.preco,
-          quantidade_comprada: customer.quantidade_comprada,
-          safra: wine.safra,
-          ano_compra: wine.ano_compra,
-          total: wine.preco * customer.quantidade_comprada,
-        })
+      customer.compras_vinho.forEach(order => {
+        wines
+          .filter(wine => wine.tipo_vinho === order.tipo)
+          .forEach(selectedWine => {
+            customerOrders.push({
+              nome: customer.nome,
+              cpf: customer.cpf,
+              telefone: customer.telefone,
+              compras_vinho: {
+                tipo: selectedWine.tipo_vinho,
+                quantidade: order.quantidade,
+                safra: selectedWine.safra,
+                preco: selectedWine.preco,
+                ano_compra: selectedWine.ano_compra,
+                total: order.quantidade * selectedWine.preco,
+              },
+            })
+          })
       })
     })
 
