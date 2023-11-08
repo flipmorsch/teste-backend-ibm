69131d979df1affbc37efa7ea022b3a9a8f1aa07
Felipe Marques <flipmorsch@gmail.com>
2023-11-08 15:13:34 -0300

    add dependency injection into services

--- src/application/services/customer.service.ts
+++ src/application/services/customer.service.ts
@@ -1,10 +1,12 @@
+import {inject, injectable} from 'tsyringe'
 import {config} from '../../config'
 import {Customer} from '../../domain/entities/customer'
 import {Http} from '../../infra/http'
 
+@injectable()
 export class CustomerService {
   private readonly customer_path = config.customer_path
-  constructor(private readonly http: Http) {}
+  constructor(@inject(Http) private readonly http: Http) {}
 
   async getCustomers() {
     const customers = await this.http.get<Customer[]>(this.customer_path)
--- src/application/services/wine.service.ts
+++ src/application/services/wine.service.ts
@@ -1,10 +1,12 @@
+import {inject, injectable} from 'tsyringe'
 import {config} from '../../config'
 import {Wine} from '../../domain/entities/wine'
 import {Http} from '../../infra/http'
 
+@injectable()
 export class WineService {
   private readonly wine_path = config.wine_path
-  constructor(private readonly http: Http) {}
+  constructor(@inject(Http) private readonly http: Http) {}
 
   async getWines() {
     const wines = await this.http.get<Wine[]>(this.wine_path)
