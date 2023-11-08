a7ed6b8422cd5cfa0d4fb5759e431e4af3b4f183
Felipe Marques <flipmorsch@gmail.com>
2023-11-07 22:51:49 -0300

    change customer path config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   base_url: 'https://run.mocky.io',
-  customer_path: '/v3/8143a06a-c200-4482-b1ca-ad5f45c114dd',
+  customer_path: '/v3/36e6b9c5-2cb2-41c2-a546-9c8966972562',
   wine_path: '/v3/37105b2e-7783-4133-bbd4-632503347c95',
 }
